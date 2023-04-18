3ab3c23c932c43ab632fab68cd330d3867e90fb4
RomanKosmyna <roma.kosmyna@gmail.com>
2023-04-18 20:34:55 +0300

    Testing 5

--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,6 @@ export default function Home({allPostsData}) {
             </Head>
             <section className={utilStyles.headingMd}>
                 <p>What is going on</p>
-                <p>
-                    (This is a sample website - you’ll be building a site like this on{' '}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-                </p>
             </section>
             <section>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
